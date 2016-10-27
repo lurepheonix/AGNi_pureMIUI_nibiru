b8114be5ec962538a59b470ea406d32fb78121af
psndna88 <psndna88@gmail.com>
2016-10-27 08:37:52 +0530

    WIP: led and goodix patch dts

--- build_kernel_kenzo.sh
+++ build_kernel_kenzo.sh
@@ -7,35 +7,52 @@ export KERNELDIR=`readlink -f .`
 #. ~/gcc-7.x-uber_aarch64.sh
 
 export ARCH=arm64
-mv .git .git-halt
 
 echo ""
 echo " Cross-compiling AGNi pureMIUI-MM kernel ..."
 echo ""
+echo "Cleanup..."
+echo ""
 cd $KERNELDIR/
-rm $KERNELDIR/arch/arm64/boot/Image
-rm $KERNELDIR/arch/arm64/boot/Image.gz
-rm $KERNELDIR/arch/arm64/boot/Image.gz-dtb
-rm $KERNELDIR/arch/arm/boot/dts/*.dtb
 
 if [ ! -f $KERNELDIR/.config ];
 then
 make agni_kenzo_defconfig
 fi
 
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
 make -j3 || exit 1
+mv .git-halt .git
 
 rm -rf $KERNELDIR/BUILT_kenzo
 mkdir -p $KERNELDIR/BUILT_kenzo/system/lib/modules/pronto
 
 find -name '*.ko' -exec mv -v {} $KERNELDIR/BUILT_kenzo/system/lib/modules/ \;
-cp $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo/
+cp $KERNELDIR/arch/arm64/boot/Image $KERNELDIR/BUILT_kenzo/
 mv $KERNELDIR/BUILT_kenzo/system/lib/modules/wlan.ko $KERNELDIR/BUILT_kenzo/system/lib/modules/pronto/pronto_wlan.ko
 
 echo ""
 echo " BUILT_kenzo is ready."
 
+echo ""
+echo "Generating Device Tree images ..."
+chmod +x $KERNELDIR/scripts/dtbtool_agni
+cd $KERNELDIR;
+# Goodix X
+$KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo/dt.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
+# Goodix V
+git apply goodix.patch && echo "   Goodix Patch applied ..."
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
 mv .git-halt .git
+$KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo/dt_goodix.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
+git apply -R goodix.patch && echo "   Goodix Patch Cleaned UP."
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+rm $KERNELDIR/arch/arm64/boot/Image
+rm $KERNELDIR/arch/arm64/boot/Image.gz
+rm $KERNELDIR/arch/arm64/boot/Image.gz-dtb
 
 echo ""
 echo "AGNi pureMIUI-MM has been built for kenzo !!!"
--- build_kernel_kenzo_cm_mm.sh
+++ build_kernel_kenzo_cm_mm.sh
@@ -7,23 +7,23 @@ export KERNELDIR=`readlink -f .`
 #. ~/gcc-7.x-uber_aarch64.sh
 
 export ARCH=arm64
-mv .git .git-halt
 
 echo ""
 echo " Cross-compiling AGNi pureCM-MM kernel ..."
 echo ""
+echo "Cleanup..."
+echo ""
 cd $KERNELDIR/
-rm $KERNELDIR/arch/arm64/boot/Image
-rm $KERNELDIR/arch/arm64/boot/Image.gz
-rm $KERNELDIR/arch/arm64/boot/Image.gz-dtb
-rm $KERNELDIR/arch/arm/boot/dts/*.dtb
 
 if [ ! -f $KERNELDIR/.config ];
 then
 make agni_kenzo-cm_defconfig
 fi
 
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
 make -j3 || exit 1
+mv .git-halt .git
 
 rm -rf $KERNELDIR/BUILT_kenzo-cm-mm
 mkdir -p $KERNELDIR/BUILT_kenzo-cm-mm/system/lib/modules/
@@ -34,16 +34,45 @@ cp $KERNELDIR/arch/arm64/boot/Image $KERNELDIR/BUILT_kenzo-cm-mm/
 echo ""
 echo " BUILT_kenzo-cm-mm is ready."
 
-mv .git-halt .git
-
 echo ""
-echo "Generating Device Tree image (dt.img) ..."
+echo "Generating Device Tree images ..."
 chmod +x $KERNELDIR/scripts/dtbtool_agni
-mv $KERNELDIR/arch/arm/boot/dts/msm8956-mtp-kenzo_cm_led.dtb $KERNELDIR/arch/arm/boot/dts/msm8956-mtp-kenzo_cm_led.dtb.bak
+cd $KERNELDIR;
+# LED X 		Goodix X
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
+mv .git-halt .git
 $KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo-cm-mm/dt.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
-rm $KERNELDIR/arch/arm/boot/dts/msm8956-mtp-kenzo_cm.dtb
-mv $KERNELDIR/arch/arm/boot/dts/msm8956-mtp-kenzo_cm_led.dtb.bak $KERNELDIR/arch/arm/boot/dts/msm8956-mtp-kenzo_cm_led.dtb
-$KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo-cm-mm/dt-cm.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
+# LED V 		Goodix X
+git apply led.patch && echo "   LED Patch applied ..."
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
+mv .git-halt .git
+$KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo-cm-mm/dt_led.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
+# LED X 		Goodix V
+git apply -R led.patch && echo "   LED Patch removed ..."
+git apply goodix.patch && echo "   Goodix Patch applied ..."
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
+mv .git-halt .git
+$KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo-cm-mm/dt_goodix.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
+# LED V 		Goodix V
+git apply led.patch && echo "   LED Patch applied ..."
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
+mv .git-halt .git
+$KERNELDIR/scripts/dtbtool_agni -s 2048 -o $KERNELDIR/BUILT_kenzo-cm-mm/dt_led_goodix.img -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/
+git apply -R led.patch && echo "   LED Patch Cleaned UP."
+git apply -R goodix.patch && echo "   Goodix Patch Cleaned UP."
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+rm $KERNELDIR/arch/arm64/boot/Image
+rm $KERNELDIR/arch/arm64/boot/Image.gz
+rm $KERNELDIR/arch/arm64/boot/Image.gz-dtb
+
 echo "AGNi pureCM-MM has been built for kenzo !!!"
 
 
